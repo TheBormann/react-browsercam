e969ed185ba26a45881f2ce757853f76b7ddddb3
Lukas Bormann <mail@lukas-bormann.com>
2022-04-06 22:04:34 +0200

    feat: added camera interface. Image detial button needs overhaul

--- src/components/camera/components/camera_interface.tsx
+++ src/components/camera/components/camera_interface.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import CaptureButton from './capture_button';
 
 /**
@@ -6,18 +6,29 @@ import CaptureButton from './capture_button';
  */
 type Props = {
   handleCapture: () => void;
-  image: 'base64' | null;
+  image: Blob | HTMLCanvasElement | string | null;
+  openImage: () => void;
 };
-//TODO change button to photobutton
-const CameraInterface = ({ handleCapture, image }: Props) => {
+const CameraInterface = ({ handleCapture, image, openImage }: Props) => {
+  const [imageBase64, setImageBase64] = useState<string | null>(null);
+  useEffect(() => {
+    if (image instanceof  Blob){
+      setImageBase64(URL.createObjectURL(image as Blob));
+    }else if ( typeof image === "string"){
+      setImageBase64(image);
+    }else if (image instanceof HTMLCanvasElement) {
+      setImageBase64(image.toDataURL());
+    }
+  }, [image]);
+
   return (
     <Fragment>
-      {image && (
+      {imageBase64 && (
+        // TODO: make image button size, dependent on parent size
         <img
           className={`absolute bottom-2 left-2 w-16 h-16 rounded-lg object-cover hover:cursor-pointer `}
-          alt="Preview Image Button"
-          src={image}
-          onClick={() => {console.log("open Details")}}
+          src={imageBase64}
+          onClick={openImage}
         />
       )}
       <div className={`absolute bottom-2 left-1/2 -translate-x-1/2 transition flex`}>
