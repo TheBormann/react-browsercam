4597f772279b1957f7e40472a849c0d379f9571d
Lukas Bormann <mail@lukas-bormann.com>
2022-04-09 20:35:40 +0200

    feat: added imageDetail to camera stories

--- src/stories/camera.stories.tsx
+++ src/stories/camera.stories.tsx
@@ -1,10 +1,11 @@
 // File: src/stories/camera.stories.tsx
 
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
 import { Camera } from '../components';
 import CameraInterface from '../components/camera/components/camera_interface';
 import { useCapture } from '../components/camera/hooks/use_capture';
+import ImgDetailPopup from '../components/camera/components/img_detail_popup';
 
 export default {
   title: 'Example/Camera',
@@ -13,11 +14,25 @@ export default {
 
 const Template: Story = (args) => {
   const { image, isAccessingCamera, videoRef, capture, setBeforeCapture } = useCapture({});
+  const [displayDetails, setDisplayDetails] = useState(false);
 
-  return <div className="w-96 h-60">
-    <Camera videoRef={videoRef} isAccessingCamera={isAccessingCamera} flash={setBeforeCapture} {...args}>
-      <CameraInterface image={image} handleCapture={capture} openImage={() => console.log("open Details")} />
-    </Camera>
-  </div>
+  return (
+    <>
+      <div className="w-60 h-60">
+        <Camera
+          videoRef={videoRef}
+          isAccessingCamera={isAccessingCamera}
+          flash={setBeforeCapture}
+          {...args}>
+          <CameraInterface
+            image={image}
+            handleCapture={capture}
+            openImage={() => setDisplayDetails(true)}
+          />
+        </Camera>
+      </div>
+      <ImgDetailPopup image={image} visible={displayDetails} handleClose={() => setDisplayDetails(false)}/>
+    </>
+  );
 };
 export const Default = Template.bind({});
