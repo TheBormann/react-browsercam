8a66cd9f4fabfc2e490ac5fa1adcf26392d3fd09
Lukas Bormann <mail@lukas-bormann.com>
2022-04-09 20:42:16 +0200

    fix: capture button is now more accessible

--- src/components/camera/components/capture_button.tsx
+++ src/components/camera/components/capture_button.tsx
@@ -8,15 +8,20 @@ type Props = {
   color?: string;
   onClick: () => void;
 };
-//TODO change button to photobutton
+//TODO change button style
 const CaptureButton = ({ onClick }: Props) => {
   return (
     <button
       onClick={onClick}
       className={`w-14 h-14 p-0 m-1 bg-transparent rounded-full focus:outline-none`}>
-      <svg className={"h-fit w-fit"} viewBox="0 0 316 316" xmlns="http://www.w3.org/2000/svg" fill='none'>
-        <circle className={`fill-white`} cx="158" cy="158" r="139" />
-        <circle className={`stroke-white`} cx="158" cy="158" r="155.5" strokeWidth="5" />
+      <svg
+        className={'h-fit w-fit'}
+        viewBox="0 0 316 316"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none">
+        <circle cx="158" cy="158" r="158" fill="#093333" fillOpacity=".4" />
+        <circle cx="158" cy="158" r="139" fill="#fff" />
+        <circle cx="158" cy="158" r="155.5" stroke="#fff" strokeWidth="5" />
       </svg>
     </button>
   );
