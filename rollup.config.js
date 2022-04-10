ada3afbe32c87961830b9cbf105f68c99705c102
Lukas Bormann <mail@lukas-bormann.com>
2022-04-10 15:07:44 +0200

    fix: fixed packaging with rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,34 @@
 import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-import dts from 'rollup-plugin-dts';
+import postcss from 'rollup-plugin-postcss';
+import { terser } from 'rollup-plugin-terser';
 
-const packageJson = require('./package.json');
+const dist = 'dist';
+
+export default {
+  input: 'src/index.ts',
+  external: ['react'],
+  output: 
+  [
+    {
+      sourcemap: true,
+      file: `${dist}/bundle.cjs.js`,
+      format: 'commonjs'
+    },
+    {
+      sourcemap: true,
+      file: `${dist}/bundle.esm.js`,
+      format: 'esm'
+    },
+    {
+      sourcemap: true,
+      name: 'ReactBrowserCam',
+      file: `${dist}/bundle.umd.js`,
+      globals: {
+        react: 'React'
+      },
+      format: 'umd'
+    }
+  ],
+  plugins: [resolve(), typescript({ tsconfig: './tsconfig.json', sourceMap: true}), terser(), postcss()]
+};
